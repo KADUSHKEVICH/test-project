d2b18f29d9898f4f3f5a7b6097d6b5c828c72c57
yankaduchkevich <kadushkevichyanolegovich@gmail.com>
2022-08-01 19:03:37 +0300

    added 'key' prop with 'index' value inside 'map' method

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,18 +2,18 @@ import * as React from "react"
 import * as s from "./List.module.css"
 
 const listItems = [
-    {key: 1, value: 'Accelerated uploading'},
-    {key: 2, value: 'Uploads from URL'},
-    {key: 3, value: 'Malware protection'},
-    {key: 4, value: 'Signed uploads'},
-    {key: 5, value: 'Platform-side MIME-type filtering'},
-    {key: 6, value: 'Up to 5 TB file size'}
+    {value: 'Accelerated uploading'},
+    {value: 'Uploads from URL'},
+    {value: 'Malware protection'},
+    {value: 'Signed uploads'},
+    {value: 'Platform-side MIME-type filtering'},
+    {value: 'Up to 5 TB file size'}
 ]
 
 export const List = ({className}) => {
     return (
         <ul className={`${s.list} ${className}`}>
-            {listItems.map(item => <li key={item.key} className={s.item}>{item.value}</li>)}
+            {listItems.map((item, index) => <li key={index} className={s.item}>{item.value}</li>)}
         </ul>
     )
 }
