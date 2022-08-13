b556a78ce56cd9cc24bbc3a14ed79184ee2ed3a8
yankaduchkevich <kadushkevichyanolegovich@gmail.com>
2022-08-13 17:06:07 +0300

    first page layout - grid-template-areas and second page layout - conditional rendering

--- src/pages/page-1.js
+++ src/pages/page-1.js
@@ -1,23 +1,15 @@
 import * as React from "react"
 import { Layout, Container, Description, List, Comment } from "../components"
-import { useIsMobile } from "../shared"
 import * as s from "../styles/page-1.module.css"
 
 const Page1 = () => {
 
-    const isMobile = useIsMobile()
-
     return (
         <Layout className={s.layout}>
             <Container className={s.container}>
-                <div className={s.left_side}>
-                    <Description/>
-                    {!isMobile && <Comment/>}
-                </div>
-                <div className={s.right_side}>
-                    <List className={s.list_wrapp}/>
-                    {isMobile && <Comment/>}
-                </div>
+                <Description className={s.descr_wrapp}/>
+                <Comment className={s.comment_wrapp}/>
+                <List className={s.list_wrapp}/>
             </Container>
         </Layout>
     )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,18 +1,22 @@
 import * as React from "react"
 import { Layout, Container, Description, List, Comment } from "../components"
+import { useIsMobile } from "../shared"
 import * as s from "../styles/page-2.module.css"
 
 const Page2 = () => {
+
+    const isMobile = useIsMobile()
+
     return (
         <Layout className={s.layout}>
             <Container className={s.container}>
                 <div className={s.left_side}>
                     <Description className={s.descr_wrapp}/>
-                    <Comment className={s.comm_wrapp}/>
+                    {!isMobile && <Comment className={s.comm_wrapp}/>}
                 </div>
                 <div className={s.right_side}>
                     <List className={s.list_wrapp}/>
-                    <Comment className={s.comm_wrapp_mobile}/>
+                    {isMobile && <Comment className={s.comm_wrapp}/>}
                 </div>
             </Container>
         </Layout>
